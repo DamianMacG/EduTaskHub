6cee78d5ee0b17b85e4055bf04be3506eacd44f8
damiang <damiangildea12@gmail.com>
2024-01-04 11:42:54 +0000

    Configured babel json and package json to finally pass the first test

--- front-end/src/components/__tests__/hello.test.tsx
+++ front-end/src/components/__tests__/hello.test.tsx
@@ -1,3 +1,12 @@
-test('basic test', () => {
-    expect(true).toBe(true)
-})
+import React from 'react';
+import { render, screen } from '@testing-library/react';
+import '@testing-library/jest-dom'
+import Hello from "../hello";
+
+
+test('Should render hello component', () => {
+    render(<Hello/>);
+    const helloComponent = screen.getByTestId("hello");
+    expect(helloComponent).toBeInTheDocument();
+    expect(helloComponent).toHaveTextContent("Hello, World!");
+  });
